90e2de20b820e16f64917ae9f88e3138df3c02d6
Christopher Snizik <csnizik@southleft.com>
2021-06-12 16:06:06 -0500

    rm unused stories n shit

--- src/patterns/05-pages/Home.js
+++ src/patterns/05-pages/Home.js
@@ -1,5 +1,5 @@
-import classNames from 'classnames';
 import React from 'react';
+import classNames from 'classnames';
 import { useLocation } from 'react-router-dom';
 import Sidebar from '../02-molecules/navigation/Sidebar';
 import Dashboard from '../04-templates/layouts/Dashboard';
@@ -8,20 +8,27 @@ const Home = () => {
   const pathName = useLocation().pathname; // Home path /
   return (
     <div
-      className={classNames(
-        'flex',
-        'flex-row',
-        'space-between-3',
-        'min-h-screen',
-        'overflow-hidden'
-      )}
-    >
-      <Sidebar pathName={pathName} />
-
-      <main className={classNames('flex-grow')}>
-        <Dashboard pathName={pathName}/>
-      </main>
+      className={classNames('container', 'bg-white-dark', 'min-h-full')}
+      id='container'>
+      <section className={classNames('')}>
+        <h1>DFWB</h1>
+      </section>
     </div>
+    // <div
+    //   className={classNames(
+    //     'flex',
+    //     'flex-row',
+    //     'space-between-3',
+    //     'min-h-screen',
+    //     'overflow-hidden'
+    //   )}
+    // >
+    //   <Sidebar pathName={pathName} />
+
+    //   <main className={classNames('flex-grow')}>
+    //     <Dashboard pathName={pathName}/>
+    //   </main>
+    // </div>
   );
 };
 
