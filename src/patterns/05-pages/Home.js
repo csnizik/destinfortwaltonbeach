a7c5ca9a4cb6c3d689ad79ac2afca8363f75faf2
Christopher Snizik <csnizik@southleft.com>
2021-06-13 18:49:40 -0500

    home page layout

--- src/patterns/05-pages/Home.js
+++ src/patterns/05-pages/Home.js
@@ -4,31 +4,34 @@ import { useLocation } from 'react-router-dom';
 import Sidebar from '../02-molecules/navigation/Sidebar';
 import Dashboard from '../04-templates/layouts/Dashboard';
 
+import './Page.css';
+
 const Home = () => {
   const pathName = useLocation().pathname; // Home path /
   return (
     <div
-      className={classNames('container', 'bg-white-dark', 'min-h-full')}
+      className={classNames(
+        'container',
+        'bg-white-dark',
+        'min-h-screen',
+        'overflow-hidden',
+        'space-between-3',
+        'flex',
+        'shadow'
+      )}
       id='container'>
-      <section className={classNames('')}>
-        <h1>DFWB</h1>
+      
+      <section
+        className={classNames('min-h-screen')}
+        id='sidebar'>
+        <Sidebar pathName={pathName} />
       </section>
-    </div>
-    // <div
-    //   className={classNames(
-    //     'flex',
-    //     'flex-row',
-    //     'space-between-3',
-    //     'min-h-screen',
-    //     'overflow-hidden'
-    //   )}
-    // >
-    //   <Sidebar pathName={pathName} />
 
-    //   <main className={classNames('flex-grow')}>
-    //     <Dashboard pathName={pathName}/>
-    //   </main>
-    // </div>
+      {/* Be aware that the active menu item's style has pseudo classes that extend pieces of off-white background beyond the sidebar's  width. They should remain safely within the main content section's padding, but if they ever overflow on top of anything in main content, fix it by setting a class of z-10 here. */}
+      <main className={classNames('min-h-screen','sm:w-full')}> 
+        <Dashboard pathName={pathName} />
+      </main>
+    </div>
   );
 };
 
