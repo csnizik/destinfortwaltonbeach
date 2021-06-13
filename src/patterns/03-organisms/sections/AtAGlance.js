ad9a6d61c015c60d7fa71b90e9cd5d6a5c15c3be
Christopher Snizik <csnizik@southleft.com>
2021-06-13 18:49:54 -0500

    spacing on ataglance

--- src/patterns/03-organisms/sections/AtAGlance.js
+++ src/patterns/03-organisms/sections/AtAGlance.js
@@ -5,64 +5,64 @@ import Metric from '../../02-molecules/blocks/Metric';
 
 const AtAGlance = ({ pathName, classes, ...props }) => {
   return (
-    <section className={classNames(classes)}>
-      <GridCard ctaText="Show More Information">
+    <GridCard ctaText='Show More Information'>
         <h2 className={classNames('hed2')}>At a Glance</h2>
         <div
           className={classNames(
-            'flex-grow',
             'flex',
-            'flex-row',
-            'justify-around',
-            'divide-x',
+            'flex-col',
+            'lg:flex-row',
+            'divide-y',
+            'divide-x-0',
+            'lg:divide-x',
+            'lg:divide-y-0',
             'divide-gray-300',
             'divide-solid',
-            'space-x-6'
-          )}
-        >
-          <div className={classNames('w-1/3')}>
-            <div
-              className={classNames(
-                'flex',
-                'flex-col',
-                'justify-between',
-                'items-center'
-              )}
-            >
-              <Metric value="12.7%" trend="up" />
-              <h3 className={classNames('hed3')}>Visa Spend Per Visitor</h3>
+            // 'xl:space-x-6'
+          )}>
+        <div
+          className={classNames(
+            'lg:w-1/3',
+            'flex',
+            'flex-row',
+            'lg:flex-col',
+            'justify-start',
+            'lg:justify-between',
+            'items-center',
+            'py-3',
+                'lg:py-0'
+              )}>
+              <Metric value='12.7%' trend='up' />
+              <h3 className={classNames('hed3','ml-2','lg:ml-0')}>Visa Spend Per Visitor</h3>
             </div>
-          </div>
 
-          <div className={classNames('w-1/3')}>
-          <div
-            className={classNames(
-              'flex',
-              'flex-col',
-              'justify-between',
-              'items-center'
-            )}
-          >
-            <Metric value="12.4%" trend="down" />
-            <h3 className={classNames('hed3')}>Visa Consumer Spend</h3>
-            </div>
-            </div>
-          <div className={classNames('w-1/3')}>
-          <div
-            className={classNames(
-              'flex',
-              'flex-col',
-              'justify-between',
-              'items-center'
-            )}
-          >
-            <Metric value="0.6%" trend="up" />
-            <h3 className={classNames('hed3')}>ROAI</h3>
+            <div className={classNames('lg:w-1/3',
+                'flex',
+                'flex-row',
+                'lg:flex-col',
+                'justify-start',
+                'lg:justify-between',
+                'items-center',
+                'py-3',
+                    'lg:py-0')}>
+              <Metric value='12.4%' trend='down' />
+              <h3 className={classNames('hed3','ml-2','lg:ml-0')}>Visa Consumer Spend</h3>
           </div>
-          </div>
+          
+            <div className={classNames('lg:w-1/3',
+                'flex',
+                'flex-row',
+                'lg:flex-col',
+                'justify-start',
+                'lg:justify-between',
+                'items-center',
+                'py-3',
+                    'lg:py-0')}>
+              <Metric value='0.6%' trend='up' />
+              <h3 className={classNames('hed3','ml-2','lg:ml-0')}>ROAI</h3>
+            </div>
           </div>
       </GridCard>
-    </section>
   );
 };
 
