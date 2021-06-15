3fed7bb5bcdaf024853a2dc444ce0fffda300ac0
Christopher Snizik <csnizik@southleft.com>
2021-06-15 09:44:19 -0500

    rm unused

--- src/patterns/03-organisms/sections/TourismConfidence.js
+++ src/patterns/03-organisms/sections/TourismConfidence.js
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React, { useState } from 'react';
+import React from 'react';
 import Card from '../../02-molecules/blocks/Card/Card';
 import Chart from 'react-google-charts';
 
